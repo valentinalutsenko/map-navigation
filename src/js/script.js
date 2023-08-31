dd419a12258d2ba879ae666ee1c0593bc6fc88a6
Valentina <lucenkovalentina@yandex.ru>
2023-08-31 16:02:56 +0300

    add button and styles

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+const btn1 = document.querySelector(".btn_1");
+const btn2 = document.querySelector(".btn_2");
+const btn3 = document.querySelector(".btn_3");
+const btn4 = document.querySelector(".btn_4");
+const btn5 = document.querySelector(".btn_5");
+const btn6 = document.querySelector(".btn_6");
+const btn7 = document.querySelector(".btn_7");
+const btn8 = document.querySelector(".btn_8");
+const btn9 = document.querySelector(".btn_9");
+const btn10 = document.querySelector(".btn_10");
+
+btn1.addEventListener("click", () => {
+  btn1.style = `
+  
+  `;
+});
