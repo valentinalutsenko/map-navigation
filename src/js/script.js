0fe90b94f631d383a64b57d03c144ca167892415
Valentina <lucenkovalentina@yandex.ru>
2023-09-03 19:38:36 +0300

    main

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,27 @@
-const btn1 = document.querySelector(".btn_1");
-const btn2 = document.querySelector(".btn_2");
-const btn3 = document.querySelector(".btn_3");
-const btn4 = document.querySelector(".btn_4");
-const btn5 = document.querySelector(".btn_5");
-const btn6 = document.querySelector(".btn_6");
-const btn7 = document.querySelector(".btn_7");
-const btn8 = document.querySelector(".btn_8");
-const btn9 = document.querySelector(".btn_9");
-const btn10 = document.querySelector(".btn_10");
+const btn = document.querySelectorAll("button");
+const spanShow = document.querySelectorAll(".show");
+const spanHide = document.querySelectorAll(".hide");
 
-btn1.addEventListener("click", () => {
-  btn1.style = `
-  
-  `;
+btn.forEach((b) => {
+  b.addEventListener("click", (event) => {
+    //add class active
+    let target = event.target;
+    target.classList.toggle("active");
+
+    //  show/hide text
+    spanShow.forEach((show) => {
+      if (b.classList.contains("active")) {
+        show.style.display = "none";
+      } else {
+        show.style.display = "block";
+      }
+    });
+    spanHide.forEach((hide) => {
+      if (b.classList.contains("active")) {
+        hide.style.display = "block";
+      } else {
+        hide.style.display = "none";
+      }
+    });
+  });
 });
